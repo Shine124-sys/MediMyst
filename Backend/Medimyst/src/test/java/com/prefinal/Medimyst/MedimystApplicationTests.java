7695042277dd825ee26678ea1c1c16d3e609d681
shoriful shine <www.shorifulshine@gmail.com>
2025-10-28 00:36:31 +0530

    in first

---
+++ Backend/Medimyst/src/test/java/com/prefinal/Medimyst/MedimystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prefinal.Medimyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
